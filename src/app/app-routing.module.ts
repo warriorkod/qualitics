f8fa6ffe689e63807b19e2a9fbfc1ecf72b0745f
Warrior <abdoulaye.kane093@gmail.com>
2019-10-09 13:55:38 +0000

    Creating UI for authentification page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+export const routes: Routes = [
+  {  
+    path: '',
+    redirectTo: '/sign_in',
+    pathMatch: 'full'
+  },
+  {
+    path: 'sign_in',
+    loadChildren: () => import('./modules/auth/auth.module').then(mod => mod.AuthModule)
+  },
+  {  
+    path: '**',
+    redirectTo: '/sign_in',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
